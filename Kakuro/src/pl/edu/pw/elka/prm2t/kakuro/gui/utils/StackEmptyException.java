594b2583fd931784872690593fb547ad1bace488
ekorotkov <01184764@pw.edu.pl>
2024-05-05 16:28:34 +0200

    Poprawa

--- Kakuro/src/pl/edu/pw/elka/prm2t/kakuro/gui/utils/StackEmptyException.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.pw.elka.prm2t.kakuro.gui.utils;
-
-
-public class StackEmptyException extends RuntimeException {
-    public StackEmptyException(String message) {
-        super(message);
-    }
-}
-
